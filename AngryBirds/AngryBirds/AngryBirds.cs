7fc1975c17ee25bf1840598845ace3c3ac99a298
TertychnyiGMH2019 <DiGri01@yandex.ru>
2021-04-08 23:02:35 +0500

    Все переделал, пока далеко не мой уровень

--- AngryBirds/AngryBirds/AngryBirds.cs
+++ AngryBirds/AngryBirds/AngryBirds.cs
@@ -1,96 +1,101 @@
 ﻿using System;
-using System.Windows;
-using System.Windows.Controls;
 using System.IO;
-
 
 namespace AngryBirds
 {
     class Trajectory
     {
-        
-        public double X, Y, MomentSpeed; // MomentSpeed = v(t) = скорость в момент t
-        public readonly double TimeStep;
-        private readonly double _angle, _speed;
-        private const double G = 9.8;
-        public Trajectory(double timeStep, double x = 0, double y = 0, double speed = 0, double angle = 0)
+        private const double g = 9.8;
+        private double[] X;
+        private double[] Y;
+        private int t;
+        public Trajectory(double ang, double v)
         {
-            X = x;
-            Y = y;
-            _speed = speed;
-            _angle = ConvertToRadians(angle);  // Будем использовать sin и cos
-            TimeStep = timeStep;
+            ang = (ang * Math.PI) / 180; 
+            double t1 = (2 * v * Math.Sin(ang)) / g;
+            int l = (int)Math.Round(t1) * 10;
+
+            t = l;
+            X= new double[l];
+            Y = new double[l];
+
+            double k;
+            for (int i = 0; i < l; i++)
+            {
+                k = i;
+                X[i] = v * Math.Cos(ang) * (k / 10);
+                Y[i] = (v * Math.Sin(ang) * (k / 10)) - (g * (k / 10) * (k / 10)) / 2;
+                if (Y[i] < 0)
+                {
+                    Y[i] = 0;
+                }
+            }
+
+
         }
-        private static double Square(double value) { return value * value; } //считаем квадрат введенного числа
-        private static double ConvertToRadians(double a) { return a * Math.PI / 180; } //переводим радианы
-        public double GetFlyTime() { return 2 * _speed * Math.Sin(_angle) / G; } //функция для подсчета время полета
-        //вводим функции счета кооординат x, y
-        private double CalculateX(double time) { return _speed * Math.Cos(_angle) * time; }
-        private double CalculateY(double time) { return _speed * Math.Sin(_angle) * time - (G * Square(time) / 2); }
-        private double CalculateSpeed(double time) //считаем скорость от времени
+
+        public void WriteToFile()
         {
-            double speedX = _speed * Math.Cos(_angle),
-                   speedY = _speed * Math.Sin(_angle);
-            return Math.Sqrt((speedY - G * time) * (speedY - G * time) + Square(speedX));
+            string path = @"..\..\..\Chtoto.csv";
+            using (StreamWriter in_File = new StreamWriter(path))
+            {
+
+                in_File.Write("t:");
+                in_File.Write(';');
+                for (int i = 0; i < t; i++)
+                {
+                    in_File.Write(i);
+                    in_File.Write(';');
+                }
+
+                in_File.WriteLine(" ");
+
+                in_File.Write("x:");
+                in_File.Write(';');
+                for (int i = 0; i < t; i++)
+                {
+                    in_File.Write(Y[i]);
+                    in_File.Write(';');
+                }
+
+                in_File.WriteLine(" ");
+
+                in_File.Write("y:");
+                in_File.Write(';');
+                for (int i = 0; i < t; i++)
+                {
+                    in_File.Write(Y[i]);
+                    in_File.Write(';');
+                }
+            }
         }
-        //посчитаем длину полета
-        private double GetFlyLength() { return Square(_speed) * Math.Sin(2 * _angle); }
-        //считаем максимальную высоту полета
-        private double GetMaxFlyHeight() { return Square(_speed * Square(Math.Sin(_angle))) / 2 * G; }
-        //все параметры в одну строку, обязательно интерполируем
-        public string CollectAllParams()
+
+        public void WriteCoordinates()
         {
-            return $"Total fly time: {GetFlyTime()}{Environment.NewLine}Total fly length: {GetFlyLength()}{Environment.NewLine}Max fly height: {GetMaxFlyHeight()}";
-        }
-        //обьеденяем все в одну функцию
-        public void CalculateCoords(double time)
-        {
-            Y = CalculateY(time);
-            X = CalculateX(time);
-            MomentSpeed = CalculateSpeed(time);
+            Console.WriteLine("t:   x:   y:");
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine("{0}:   {1}   {2}", i, X[i], Y[i]);
+            }
         }
     }
-    public partial class MainWindow
+    class Program
     {
-        private static void StartVisualization(Trajectory myPoint, FrameworkElement mainGrid)
+        static void Main(string[] args)
         {
-            //переменные вводим
-            const string filename = "chtopoluchilos.out"; //обзываем файл
-            double currentTime = 0;
-            //получаем элемент TextBlock и убираем его, если он есть
-            var chtopoluchilos = (TextBlock)mainGrid.FindName("chtopoluchilos");
-            if (chtopoluchilos != null) chtopoluchilos.Text = "";
-            //Начинаем работу с файлом
-            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory()).Parent; // создаем путь для файла
-            if (directoryInfo == null) return; //файла нет
-            var path = Path.Combine(directoryInfo.FullName, filename);
-            //считаем позицию до падения
-            using (var fileStream = new StreamWriter(path))
+            double ang, v;
+            ang = Convert.ToDouble(Console.ReadLine());
+            v = Convert.ToDouble(Console.ReadLine());
+
+            if (ang >= 90 || v <= 0)
             {
-                string line;
-                if (chtopoluchilos != null)
-                {
-                    while (currentTime < myPoint.GetFlyTime())
-                    {
-                        currentTime += myPoint.TimeStep;
-
-                        myPoint.CalculateCoords(Math.Round(currentTime, 3));
-
-                        line = $"Time (t): {Math.Round(currentTime, 3)}\tX coord: {myPoint.X}\tY coord: {myPoint.Y}\tSpeed at moment t: {myPoint.MomentSpeed}";
-
-                        if (!(myPoint.Y >= 0)) continue;
-                        fileStream.WriteLine(line);
-                        chtopoluchilos.Text += line + Environment.NewLine;
-                    }
-                }
-                line = $"Upal( (At {myPoint.GetFlyTime()} seconds)";
-                fileStream.Write(line);
-                if (chtopoluchilos != null) chtopoluchilos.Text += line;
+                Console.WriteLine("Кидайте под углом не больше pi/2, с положительной скоростью");
             }
-            var paramsOutput = (TextBlock)mainGrid.FindName("ParamsOutput");
-            if (paramsOutput != null) paramsOutput.Text = myPoint.CollectAllParams();
+
+            Trajectory myPoint = new Trajectory(ang, v);
+
+            myPoint.WriteToFile();
 
         }
-
     }
 }
