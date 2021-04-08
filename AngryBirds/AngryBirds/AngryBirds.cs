216d5902cd217f3282e7e22dd0d7607f74f96af0
TertychnyiGMH2019 <DiGri01@yandex.ru>
2021-04-08 18:12:41 +0500

    class trajectore start

---
+++ AngryBirds/AngryBirds/AngryBirds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace AngryBirds
+{
+    class Trajectory
+    {
+            public double X, Y, MomentSpeed;                  // MomentSpeed = v(t) == скорость в момент t
+            public readonly double TimeStep;
+            private readonly double _angle, _speed;
+            private const double G = 9.81;
+    }
+}
